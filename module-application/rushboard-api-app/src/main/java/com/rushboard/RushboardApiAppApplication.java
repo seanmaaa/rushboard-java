ebfb7fb2fee5519a1276b521f1e247dc32d33ea9
seanmaaa <kr.seunghyeon@gmail.com>
2021-03-26 19:54:46 +0900

    main method for rushboard-api-app module

---
+++ module-application/rushboard-api-app/src/main/java/com/rushboard/RushboardApiAppApplication.java
@@ -0,0 +1,12 @@
+package com.rushboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushboardApiAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RushboardApiAppApplication.class, args);
+  }
+}
